585dfaa096d47fe64e60be0dfe503d65e24144e3
Kim Hauser <knhauser1s@gmail.com>
2025-09-14 03:09:43 -0500

    2nd commit

--- practice.js
+++ practice.js
@@ -1,1 +1,57 @@
+/*
 
+Instructions
+You are working as a developer for a local café that needs a system to track
+customer orders. The café serves drinks and pastries, and orders are stored in a
+2D array, where each row represents a category (drinks or pastries), and each
+column represents an order within that category.
+Your task is to use the .length property to track the number of items in the
+café’s order system dynamically.
+Tasks
+Task 1: Create the Order System
+Create a multi-dimensional array called orders with two rows:
+●
+●
+The first row stores three drink orders (e.g., “Latte”, “Tea”, “Espresso”).
+The second row stores three pastry orders (e.g., “Croissant”, “Muﬃn”,
+“Bagel”).
+Task 2: Log the number of drinks and number of pastries by using .length on each
+row.
+Task 3: Access Orders Using Bracket Notation
+Use bracket notation to log a specific drink and a specific pastry using
+hardcoded numbers. For example, you might want to log the first drink and last
+pastry. Do this for three combinations.
+Task 4: Access Orders Dynamically with Variables
+1
+Task 5: Write a loop that logs all the items in the drink category, ensuring the loop
+dynamically adjusts to the number of items using .length.
+Task 6: Add a New Order & Track Length
+Suppose a new order has been placed: a customer ordered a flat white. Add “flat
+white” to the drinks category dynamically. Log the updated number of drinks after
+the addition.
+Declare two variables and use them with bracket notation to log the selected order
+dynamically.
+
+*/
+
+const orders = [["latte", "tea", "espresso"],
+                ["croissant", "muffin", "bagel",]
+];
+
+let drinks = orders[0];
+let pastries = orders[1];
+
+console.log(drinks.length);
+console.log(pastries.length);
+
+console.log(drinks[0], pastries[2]);
+console.log(drinks[1], pastries[1]);
+console.log(drinks[2], pastries[0]);
+
+for (let d = 0; d < drinks.length; d++) {
+    console.log(drinks[d]);
+}
+
+moarDrinks = drinks.push("flat white");
+console.log(drinks);
+console.log(moarDrinks);
